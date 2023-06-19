e406db7227b50dd51fcd70e3cffe4ed844ab6d84
Junda Ai <39738304+Alan052918@users.noreply.github.com>
2023-06-18 21:14:33 -0700

    Update lsp.lua and treesitter.lua

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,5 +1,21 @@
 local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
-lsp.setup()
 
+lsp.ensure_installed({
+	'clangd',     -- C/C++
+	'rust_analyzer', -- Rust
+})
+
+-- Fix undefined global 'vim'
+lsp.configure('sumneko_lua', {
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { 'vim' }
+			}
+		}
+	}
+})
+
+lsp.setup()
