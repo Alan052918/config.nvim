d4cf184f4462a3a2b24ec10bcd4c6986d7b3daa4
Junda Ai <39738304+Alan052918@users.noreply.github.com>
2023-06-18 20:46:48 -0700

    Fix packer.lua

--- lua/jd/packer.lua
+++ lua/jd/packer.lua
@@ -11,28 +11,41 @@ vim.cmd [[
 	augroup end
 ]]
 
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+		vim.cmd [[packadd packer.nvim]]
+		return true
+	end
+	return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
 
-    -- telescope (fizzy finder)
-    use {
-	    'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	    -- or                            , branch = '0.1.x',
-	    requires = { {'nvim-lua/plenary.nvim'} }
-    }
+	-- telescope (fizzy finder)
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.1',
+		-- or                            , branch = '0.1.x',
+		requires = { { 'nvim-lua/plenary.nvim' } }
+	}
 
-    -- harpoon (fast file switcher)
-    use 'ThePrimeagen/harpoon'
+	-- harpoon (fast file switcher)
+	use 'ThePrimeagen/harpoon'
 
-    -- treesitter (enhanced syntax highlighting)
-    use {
-	    'nvim-treesitter/nvim-treesitter',
-	    run = ':TSUpdate'
-    }
-    use 'nvim-treesitter/playground'
+	-- treesitter (enhanced syntax highlighting)
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		run = ':TSUpdate'
+	}
+	use 'nvim-treesitter/playground'
 
-	-- fast comment 
+	-- fast comment
 	-- use 'tpope/vim-commentary'
 	use {
 		'numToStr/Comment.nvim',
@@ -41,11 +54,11 @@ return require('packer').startup(function(use)
 		end
 	}
 
-    -- undotree (file undo tree)
-    use 'mbbill/undotree'
+	-- undotree (file undo tree)
+	use 'mbbill/undotree'
 
-    -- fugitive
-    use 'tpope/vim-fugitive'
+	-- fugitive
+	use 'tpope/vim-fugitive'
 
 	-- lualine
 	use {
@@ -56,48 +69,53 @@ return require('packer').startup(function(use)
 	-- fancy startup screen
 	use 'mhinz/vim-startify'
 
-    -- lsp
-    use {
-	    'VonHeikemen/lsp-zero.nvim',
-	    branch = 'v1.x',
-	    requires = {
-		    -- LSP Support
-		    {'neovim/nvim-lspconfig'},             -- Required
-		    {'williamboman/mason.nvim'},           -- Optional
-		    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+	-- lsp
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v1.x',
+		requires = {
+			-- LSP Support
+			{ 'neovim/nvim-lspconfig' },    -- Required
+			{ 'williamboman/mason.nvim' },  -- Optional
+			{ 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-		    -- Autocompletion
-		    {'hrsh7th/nvim-cmp'},         -- Required
-		    {'hrsh7th/cmp-nvim-lsp'},     -- Required
-		    {'hrsh7th/cmp-buffer'},       -- Optional
-		    {'hrsh7th/cmp-path'},         -- Optional
-		    {'saadparwaiz1/cmp_luasnip'}, -- Optional
-		    {'hrsh7th/cmp-nvim-lua'},     -- Optional
+			-- Autocompletion
+			{ 'hrsh7th/nvim-cmp' }, -- Required
+			{ 'hrsh7th/cmp-nvim-lsp' }, -- Required
+			{ 'hrsh7th/cmp-buffer' }, -- Optional
+			{ 'hrsh7th/cmp-path' }, -- Optional
+			{ 'saadparwaiz1/cmp_luasnip' }, -- Optional
+			{ 'hrsh7th/cmp-nvim-lua' }, -- Optional
 
-		    -- Snippets
-		    {'L3MON4D3/LuaSnip'},             -- Required
-		    {'rafamadriz/friendly-snippets'}, -- Optional
-	    }
-    }
+			-- Snippets
+			{ 'L3MON4D3/LuaSnip' },    -- Required
+			{ 'rafamadriz/friendly-snippets' }, -- Optional
+		}
+	}
 
-    -- rose-pine color scheme
-    use {
-	    'rose-pine/neovim',
-	    as = 'rose-pine',
-	    config = function()
-		    require('rose-pine').setup()
-		    vim.cmd('colorscheme rose-pine')
-	    end
-    }
+	-- rose-pine color scheme
+	use {
+		'rose-pine/neovim',
+		as = 'rose-pine',
+		config = function()
+			require('rose-pine').setup()
+			vim.cmd('colorscheme rose-pine')
+		end
+	}
 
-    -- vscode color scheme
-    use {
-		'Mofiqul/vscode.nvim'
+	-- vscode color scheme
+	use {
+		'Mofiqul/vscode.nvim',
 		as = 'vscode',
 		config = function()
 			require('vscode').setup()
 			vim.cmd('colorscheme vscode')
 		end
 	}
-end)
 
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if packer_bootstrap then
+		require('packer').sync()
+	end
+end)
